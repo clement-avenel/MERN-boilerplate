db7d246834f92f0cc8b3472b1e932ecf0c43ff31
Clement Avenel <clement@squadracer.com>
2022-10-23 14:39:31 +0200

    Update project config

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DB}`,
+      `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}/${process.env.DB}`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
